a64faa8d9ce61dce333468e23de0edc4f1f0a0b1
Luís Gouveia <luisgouveia74@gmail.com>
2024-07-08 11:18:55 -0300

    Exercicio Idades finalizado

---
+++ idades.py
@@ -0,0 +1,9 @@
+nome1 = input("Digite o nome da primeira pessoa: ")
+idade1 = int(input("Digite a idade da primeira pessoa: "))
+nome2 = input("Digite o nome da segunda pessoa: ")
+idade2 = int(input("Digite a idade da segunda pessoa: "))
+
+media = (idade1 + idade2) / 2
+
+print(f"A idade media ente {nome1} e {nome2} é de {media:.1f}")
+
