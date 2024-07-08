248068cde0a4c2584c0acb2f3c40ebf81294ff34
Luís Gouveia <luisgouveia74@gmail.com>
2024-07-08 10:46:01 -0300

    Create terreno.py

---
+++ terreno.py
@@ -0,0 +1,7 @@
+largura = input(int("Digite a largura do terreno: "))
+comprimento = input(int("Digite o comprimento do terreno: "))
+
+area = largura * comprimento
+
+print(f"A area do terreno é de {area}")
+
