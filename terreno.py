0f5e936bc3e7bf78072a7907dcb5eabac2252540
Luís Gouveia <luisgouveia74@gmail.com>
2024-07-08 11:04:45 -0300

    Exercicio Terreno finalizado

--- terreno.py
+++ terreno.py
@@ -1,7 +1,10 @@
-largura = input(int("Digite a largura do terreno: "))
-comprimento = input(int("Digite o comprimento do terreno: "))
+largura = int(input("Digite a largura do terreno: "))
+comprimento = int(input("Digite o comprimento do terreno: "))
+valor = float(input("Digite o valor do m2: "))
 
 area = largura * comprimento
+preco = valor * area
 
-print(f"A area do terreno é de {area}")
+print(f"A area do terreno é de {area} m2 ")
+print(f"O valor do terreno é de R$ {preco:.2f} reais")
 
