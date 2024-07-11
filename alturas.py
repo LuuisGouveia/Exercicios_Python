dc06213f761b1087e883d7cc1d6d2f0b8620c4f9
Luís Gouveia <luisgouveia74@gmail.com>
2024-07-11 16:42:50 -0300

    exercicio alturas ainda em produção

---
+++ alturas.py
@@ -0,0 +1,8 @@
+n = int(input("Quantas pessoas serão digitadas?"))
+
+for i in range(n):
+    print(f"Digite os dados da {i}a Pessoa: ")
+    nome = input("Nome: ")
+    idade = input("Idade: ")
+    altura = input("Altura: ")
+    
